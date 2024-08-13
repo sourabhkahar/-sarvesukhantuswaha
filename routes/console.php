6fd30bb11d7ee99593abb5b4cd7a4bbaac0f5bec
sourabhkahar <sourabhkahar8140@gmail.com>
2024-08-13 10:06:29 +0530

    Fixed -  Artisan comand issue in scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('emails:work')->everyMinute()->withoutOverlapping();
+Schedule::command('emails:work')->everyMinute()->withoutOverlapping();
